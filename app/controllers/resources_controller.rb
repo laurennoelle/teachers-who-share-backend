8151c682cfebe06991ca7b92416b84aff7c3df59
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-09-12 17:26:25 -0600

    form works with routes

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -9,4 +9,25 @@ class ResourcesController < ApplicationController
         resource = Resource.find(params[:id])
         render json: resource, status: :ok
     end
+
+    def create
+        resource = Resource.create(resource_params)
+        render json: resource, status: :created
+    end
+
+    def destroy
+        resource = Resource.find_by(id:params[:id])
+        if resource
+            resource.destroy
+            head :no_content
+        else
+            render json: {error: "resource not found"}, status: not_found
+        end 
+    end 
+
+    private
+
+    def resource_params
+        params.permit(:title, :subject, :grade, :description, :reviews, :image)
+    end
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,2 +1,6 @@
 class Resource < ApplicationRecord
+    validates :title, :subject, :description, :image, presence: true
+    validates :grade, numericality: {greater_than: 0}
+    validates :reviews, numericality: {less_than: 200}
+    
 end
